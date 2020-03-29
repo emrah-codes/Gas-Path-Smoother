c9dbcaf6ad8afa7855e18c1c9cc8253311ff971b
Emrah <62834380+emrah-codes@users.noreply.github.com>
2020-03-29 14:21:15 +0300

    Update GasPathSmoother.py

--- GasPathSmoother.py
+++ GasPathSmoother.py
@@ -1,6 +1,6 @@
 '''
 A gas-path smoothing tool using B-spline curves
-First B-spline curvea are created fitting the given coordinates
+First B-spline curves are created fitting the given coordinates
 Then, the user can adjust control points to modify these curves
 '''
 
